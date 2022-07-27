9b3d3addd2e07fad1f0de7a8a9c3588a095f120b
Jyotendra <85470769+jyotendra02@users.noreply.github.com>
2022-07-27 23:33:12 +0530

    contructor destructor

---
+++ contructor-destructor.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Riya
+{
+     int a,b;
+     public: 
+     Riya(int x , int y)  // This is Constructor 
+     {
+       a=x;
+       b=y;
+     }   
+    ~Riya()                // This is Destructor
+    {
+    cout<<"\nObject is destroyed";
+    }
+    void show()
+    {
+       cout<<"Value of a = "<<a<<"\nValue of b = "<<b;  
+    }
+};
+
+/*Riya::Riya(int x , int y)
+{
+    a=x;
+    b=y;
+}
+
+Riya::~Riya()
+{
+    cout<<"\nObject is destroyed";
+}
+
+void Riya::show()
+{
+    cout<<"Value of a = "<<a<<"\nValue of b = "<<b;
+}*/
+
+
+int main()
+{
+    Riya obj(18,19);
+    obj.show();
+    return 0;
+}
